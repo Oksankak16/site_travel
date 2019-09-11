f4689fa9ca3f636ce311b2f7e9a4cd12c5aa1436
Oksana Kosyieva <oksankak16@gmail.com>
2019-09-11 21:19:18 +0000

    slider

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// // create slider
+// let slides = document.querySelectorAll('#slides .slide');
+// let currentSlide = 0;
+// let slideInterval = setInterval(nextSlide, 2000);
+
+// function nextSlide() {
+//     slides[currentSlide].className = 'slide';
+//     currentSlide = (currentSlide + 1) % slides.length;
+//     slides[currentSlide].className = 'slide showing';
+// }
+// create clock
+const clock = document.querySelector('#clock');
+setInterval(()=>clock.innerText = (new Date()).toLocaleString(), 1000);
+
+// dialog
+let dialog = document.querySelector('dialog');
+document.querySelector('#show').addEventListener('click', ()=>dialog.show());
+
+//slider
+$('.carousel').carousel({
+  interval: 2000
+})
