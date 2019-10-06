be570b7f796b283d1a3e0c5e5f06ca6262e31d7a
Oksana Kosyieva <oksankak16@gmail.com>
2019-10-06 18:31:38 +0000

    change_logo

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // // create slider
 // let slides = document.querySelectorAll('#slides .slide');
 // let currentSlide = 0;
@@ -14,8 +13,6 @@ const clock = document.querySelector('#clock');
 setInterval(()=>clock.innerText = (new Date()).toLocaleDateString(), 1000);
 
 // dialog
-=======
->>>>>>> 56897576b6f564d391f926a7b39e93038bf21f59
 let dialog = document.querySelector('dialog');
 document.querySelector('#show').addEventListener('click', ()=>dialog.show());
 document.querySelector('#close').onclick = function() {
